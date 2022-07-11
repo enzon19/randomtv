a84792bc7576c113a8fdbfd381f3de65c2472e57
enzon19 <89558747+enzon19@users.noreply.github.com>
2022-07-11 16:42:27 -0300

    11071641: Settings now work, they save changes!

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,46 @@
 'use strict';
 
-chrome.storage.sync.set({"": value}, function() {
-  console.log('Value is set to ' + value);
-});
+async function updateSettings() {
+  const options = ['displayType'];
+  const checkboxes = ['showMethod'];
+  const data = await chrome.storage.sync.get([...options, ...checkboxes]);
 
-chrome.storage.sync.get(['key'], function(result) {
-  console.log('Value currently is ' + result.key);
-});
+  options.forEach(option => {
+    for (const element of document.getElementsByClassName(option)) {
+      element.addEventListener('click', () => setOption(option, element.id));
+    }
+    applyEffectOnOption(option, `${option}-${data[option]}`);
+  });
+  
+  checkboxes.forEach(checkbox => {
+    const checkboxElement = document.getElementById(checkbox);
+    checkboxElement.checked = data[checkbox];
+    checkboxElement.addEventListener('click', () => setCheckbox(checkbox));
+  });
+}
+
+function applyEffectOnOption(option, elementId) {
+  for (const element of document.getElementsByClassName(option + "-applied")) {
+    element.classList.remove(option + "-applied")
+  }
+  const optionElement = document.getElementById(elementId);
+  optionElement.classList.add(option + "-applied");
+}
+
+function setOption(option, elementId) {
+  const optionElement = document.getElementById(elementId);
+  let newData = {};
+  newData[elementId.split("-")[0]] = elementId.split("-")[1];
+  console.log(newData)
+  chrome.storage.sync.set(newData);
+  applyEffectOnOption(option, elementId);
+}
+
+function setCheckbox(elementId) {
+  const checkboxElement = document.getElementById(elementId);
+  let newData = {};
+  newData[elementId] = checkboxElement.checked;
+  chrome.storage.sync.set(newData);
+}
+
+updateSettings(); 
