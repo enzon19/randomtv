c3be4f1fec314b4b521663337d1d99d264ef908c
Enzo <enzonbarata@outlook.com>
2025-05-25 14:23:18 -0300

    fix: tooltip, page as allowed param and params from url not working & feat: api method

--- js/index.js
+++ js/index.js
@@ -50,11 +50,18 @@ function appendButton(url) {
   tooltip.style.cssText =
     "position:absolute;z-index:1;bottom:100%;left:163%;margin-left:-60px;opacity:0;transition:opacity 0.2s ease;";
 
-  // Append tooltip to span
-  span.appendChild(tooltip);
+  // Show/hide on hover
+  span.addEventListener("mouseenter", () => {
+    tooltip.style.opacity = "1";
+  });
+  span.addEventListener("mouseleave", () => {
+    tooltip.style.opacity = "0";
+  });
 
   // Add click behavior
   span.addEventListener("click", async () => {
+    if (span.classList.contains("randomTV-loading")) return;
+
     const { username, apiRoute, filterAndSort } = getParamsFromURL(url);
 
     const methods = {
@@ -66,6 +73,9 @@ function appendButton(url) {
     methods[method](username, apiRoute, filterAndSort);
   });
 
+  // Append tooltip to span
+  span.appendChild(tooltip);
+
   // Inject into the page
   const target = document.querySelector(".dropdown.filter-dropdown");
   if (target) {
@@ -75,7 +85,7 @@ function appendButton(url) {
 
 function getParamsFromURL(url) {
   const params = url.match(
-    /\/users\/(.*?)\/(watchlist|favorites|lists|recommendations)(?:\/([^\/\?]+))?\?(.*)/
+    /.*\/users\/(.*?)\/(watchlist|favorites|lists|recommendations)(?:\/([^\/\?]+))?(?:\?(.*))?/
   );
 
   try {
@@ -96,7 +106,7 @@ function getParamsFromURL(url) {
       filterAndSort: Object.fromEntries(new URLSearchParams(filterAndSort)),
     };
   } catch (e) {
-    console.error("Error getting params from URL: ", url);
+    console.error("Error getting params from URL: ", url, e);
   }
 }
 
@@ -108,7 +118,7 @@ async function chooseMethod(filterAndSort) {
   // se tiver outras coisas filtrando, adeus!
   if (
     Object.keys(filterAndSort).some(
-      (e) => !["sort", "display", "fade"].includes(e)
+      (e) => !["sort", "display", "fade", "page"].includes(e)
     )
   )
     method = "local";
@@ -116,21 +126,19 @@ async function chooseMethod(filterAndSort) {
   // se não tiver mais de uma página, adeus!
   if (!document.querySelector(".pagination-top").textContent) method = "local";
 
-  //se for highlight, adeus!
-  const displayType =
-    (await chrome.storage.sync.get("displayType"))?.displayType || "modal";
-  if (displayType === "highlight") method = "local";
-
   return method;
+}
+
+function getRandomItem(items) {
+  const luckyNumber = Math.floor(Math.random() * items.length);
+  return items[luckyNumber];
 }
 
 function pickItemLocally() {
   const items = Array.from(document.querySelectorAll(".grid-item"));
 
   if (items.length > 0) {
-    const luckyNumber = Math.floor(Math.random() * items.length);
-    const pickedItem = items[luckyNumber];
-
+    const pickedItem = getRandomItem(items);
     const title = pickedItem
       .querySelector("div.poster")
       .getAttribute("data-original-title");
@@ -145,14 +153,69 @@ function pickItemLocally() {
   }
 }
 
-async function pickItemAPI() {
-  /* 
-    https://api.trakt.tv/users/id/ | lists/list_id/items | /type/sort_by/sort_how
-    https://api.trakt.tv/users/id/ | watchlist | /type/sort_by/sort_how
-    https://api.trakt.tv/users/id/ | favorites | /type/sort_by/sort_how (recommendations)
-  */
-  console.log("api");
+async function pickItemAPI(username, route, filterAndSort) {
+  document.querySelector("#randomtv-button").classList.add("randomTV-loading");
+
+  const type = filterAndSort.display;
+  chrome.runtime.sendMessage({
+    action: "apiRequest",
+    username,
+    route,
+    type,
+  });
 }
+
+// finishing the work of pickItemAPI()
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action == "apiResponse") {
+    const pickedItem = getRandomItem(message.response);
+    const pickedItemType = pickedItem.type;
+    const pickedItemMetadata = pickedItem[pickedItemType];
+
+    const title =
+      pickedItemMetadata.title ||
+      pickedItemMetadata.name ||
+      "Season " + pickedItemMetadata.number;
+
+    const subtitle = ["season", "episode"].includes(pickedItemType)
+      ? pickedItem.show.title
+      : undefined;
+
+    let url = "https://trakt.tv/";
+    if (pickedItemType === "person") {
+      url += "people/" + pickedItemMetadata.ids.slug;
+    } else if (pickedItemType === "season") {
+      url += `shows/${pickedItem.show.ids.slug}/seasons/${pickedItemMetadata.number}`;
+    } else if (pickedItemType === "episode") {
+      url += `shows/${pickedItem.show.ids.slug}/seasons/${pickedItemMetadata.season}/episodes/${pickedItemMetadata.number}`;
+    } else {
+      url += `${pickedItemType}s/${pickedItemMetadata.ids.slug}`;
+    }
+
+    const cover =
+      "https://" +
+      (pickedItemMetadata?.images?.poster?.[0] ||
+        pickedItem.show?.images?.poster?.[0] ||
+        pickedItemMetadata?.images?.headshot?.[0]);
+
+    const element = document.querySelector(
+      `[data-list-item-id="${pickedItem.id}"]`
+    );
+
+    showItem(title, subtitle, url, cover, "API", element);
+  } else if (message.action == "apiError") {
+    console.error(
+      "ERROR ON RANDOMTV API METHOD. USING LOCAL INSTEAD.",
+      message.status,
+      message.error
+    );
+    pickItemLocally();
+  }
+
+  document
+    .querySelector("#randomtv-button")
+    .classList.remove("randomTV-loading");
+});
 
 async function showItem(title, subtitle, url, cover, method, pickedElement) {
   const displayType =
@@ -244,6 +307,9 @@ function highlight(
   showMethod,
   pickedElement
 ) {
+  if (!pickedElement)
+    return showModal(title, subtitle, url, cover, method, showMethod);
+
   removeHighlights();
   if (showMethod) alert(method);
 
